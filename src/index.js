b470b27e9800b4a2a6abd9f721d411d471949824
kenyounot123 <kenlu519@gmail.com>
2024-03-14 23:12:08 -0400

    Update search bar to be a form input

--- src/index.js
+++ src/index.js
@@ -6,16 +6,17 @@ import {
 import fetchCurrentData from "./fetchData";
 import "./stylesheets/style.css";
 
+initialize();
+
 async function initialize() {
   try {
     const data = await fetchCurrentData();
     const currentTemp = data.current.feelslike_f;
     const currentDescription = data.current.condition.text;
-    const currentLocation = data.location.name + " , " + data.location.region;
+    const currentLocation = `${data.location.name}, ${data.location.region}`;
     createWeatherElements(currentLocation, currentDescription, currentTemp);
   } catch (error) {
     createErrorElement();
     showError(`${error}`);
   }
 }
-initialize();
